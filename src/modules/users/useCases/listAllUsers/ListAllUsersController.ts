c9d2413aa103febcc024d483aeaf3c1c37154bcc
windstonp <joaor4e@gmail.com>
2022-07-30 20:09:46 -0300

    finaliza desafio

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id: `${user_id}` });
+      return response.status(200).json(users);
+    } catch (e) {
+      return response.status(400).json({
+        error: e.message,
+      });
+    }
   }
 }
 
